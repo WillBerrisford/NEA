d21a0515a5d0c7809d28857e00e84e487fde24a1
Will Berrisford <willberrisford@gmail.com>
2019-02-03 22:36:10 +0000

    Sign In Working

--- NEA/Account.cs
+++ NEA/Account.cs
@@ -12,6 +12,7 @@ namespace NEA
     class Account : Database_Connect
     {
         private string AccountName { get; set; }
+        private string AccountID { get; set; }
         private bool SignedIn { get; set; }
         private string Password { get; set; }
 
@@ -20,8 +21,7 @@ namespace NEA
         
         public void SignIn(string name, string password)
         {
-            Salt_Hash Hash_Salt = Hash(password);
-            string command_text = @"SELECT PassHash FROM Users_2 " +
+            string command_text = @"SELECT ID,PassHash,Salt FROM Users_2 " +
                 "WHERE UserNames = @name";
 
             SqlConnection connection = Connect();
@@ -35,15 +35,19 @@ namespace NEA
                 {
                     while (reader.Read())
                     {
-                        if (Hash_Salt.Get_Salt() == reader["PassHash"].ToString())
+                        string The_Hash = Hash(password, Salt(reader["Salt"].ToString(), true)).Get_Hash();
+                        Debug.WriteLine("The Stored Salt:" + reader["Salt"].ToString());
+                        Debug.WriteLine("Hash1 " + The_Hash + " \nHash2 " + reader["PassHash"].ToString());
+                        if (The_Hash == reader["PassHash"].ToString())
                         {
                             SignedIn = true;
-                            Debug.WriteLine("User Has been signed in");
+                            AccountName = reader["PassHash"].ToString();
+                            AccountID = reader["ID"].ToString();
+                            Debug.WriteLine("Sign in successful");
                         }
                     }
-
-                    connection.Close();
                 }
+                connection.Close();
             }
             catch (Exception Error)
             {
@@ -51,17 +55,31 @@ namespace NEA
             }
         }
 
-        public Salt_Hash Hash(string password)
+        public byte[] Salt(string The_Salt, bool using_salt)
         {
-            byte[] salt;
-            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            if (using_salt == true)
+            {
+                byte[] salt = Convert.FromBase64String(The_Salt);
+                return salt;
+            }
+            else
+            {
+                byte[] salt;
+                new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+                return salt;
+            }
+        }
+
+        public Salt_Hash Hash(string password, byte[] salt)
+        {
+
             var PBKDF2_Salted_Hash = new Rfc2898DeriveBytes(password, salt, 15000);
 
             byte[] hashed_bytes = new byte[36];
             Array.Copy(salt, 0, hashed_bytes, 0, 16);
             Array.Copy(PBKDF2_Salted_Hash.GetBytes(20), 0, hashed_bytes, 16, 20);
             string Pass_Hash = Convert.ToBase64String(hashed_bytes);
-            Debug.WriteLine(Pass_Hash);
+            Debug.WriteLine("\nSalt Used:" + Convert.ToBase64String(salt));
 
             Salt_Hash Salt_and_Hash = new Salt_Hash(Convert.ToBase64String(salt), Pass_Hash);
             return Salt_and_Hash;
@@ -69,18 +87,17 @@ namespace NEA
 
         public void add_user(string name, string password)
         {
-            Salt_Hash Hash_Salt = Hash(password);
+            Salt_Hash Hash_Salt = Hash(password, Salt(null, false));
             string command_text = @"INSERT INTO Users_2 (UserNames, PassHash, Salt) " +
                 "Values ('" + name + "', '" + Hash_Salt.Get_Hash() + "', '" + Hash_Salt.Get_Salt() + "')";
 
             SqlConnection connection = Connect();
             try
-            {
+            { 
                 SqlCommand command = new SqlCommand(command_text, connection);
                 connection.Open();
                 SqlDataReader read = command.ExecuteReader();
                 connection.Close();
-
             }
 
             catch (Exception error)
